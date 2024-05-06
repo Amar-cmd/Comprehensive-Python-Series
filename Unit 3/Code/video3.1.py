be73a753ef4ad8d170e730970044ad691bf8d0e7
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2024-05-06 23:51:22 +0530

    Add files via upload

---
+++ Unit 3/Code/video3.1.py
@@ -0,0 +1,134 @@
+# ! Creating Variable
+age = 30
+
+# print(id(age))
+
+# 30 = age Never do this
+height = 5.9
+name = "Mogli"
+
+# name = mogli
+
+# ! Printing a variable
+# print(name)
+# print(height)
+# print(age)
+
+
+# ! Deleting Variable
+del age
+
+# print(age)
+
+del name, height
+
+# print(name)
+
+name = 12.5
+# print(name)
+
+
+# ! Type of Data
+# integer, float, string
+
+# print(type(name))
+
+
+# age, aGE, AGE, Age -> 4 alag alag variables hain
+
+age = 4
+aGE = 44
+
+# print(age)
+# print(aGE)
+#
+# print(age == aGE)
+
+
+sdata = "dac"
+# "", '' ``
+# print(sdata)
+
+# ! Multiple Data Assignment
+
+# a = 20
+# b = 30
+
+a, b = 20, "banana"
+# a = 20
+# b = "banana"
+# print(a, b)
+
+
+c = d = 30
+# c = 30
+# d = 30
+# print(c, d)
+
+# ! Type Casting
+
+# print(type(c))
+# print(type(str(c)))
+
+e = 10.5
+# print(type(e))
+# print(type(int(e)))
+
+G_9 = 30
+# print(G_9)
+
+# age, AGE, aGE different
+# for = 30
+# print(for)
+
+    # for, while, if, as, import etc
+
+x = 10
+x = 20
+# print(x, x)
+
+
+# camelCase, totalAmount -> Camel case
+
+# PascalCase, TotalAmount -> Pascal Case
+
+# snake_case, total_amount -> Snake Case
+
+# SCREAMING_SNAKE_CASE
+
+PIE = 3.14
+TOTAL_ANGLE_OF_TRIANGLE = 180
+
+print(PIE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
