f007baccfe9d9ebae8a136b1b5ea37ec7ce2d4f1
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2024-05-22 23:37:13 +0530

    Add files via upload

---
+++ Unit 3/Code/video3.2.py
@@ -0,0 +1,25 @@
+# ! Escape Sequence Examples
+
+# 1. New Line -> \n
+print("Hello\n\nWorld")
+
+# 2. Horizontal Tab -> \t
+print("Hello\t\tWorld")
+
+# 3. Backslash -> \\
+print("Hello\\World")
+
+# 4. Double Quote -> \"
+print("Hello \"World\"")
+
+# 5. Single Quote -> \'
+print("Hello \'World\'")
+
+# 6. BackSpace -> \b
+print("Hello\b\b\bWorld")
+
+# 7. Carriage Return -> \r
+print("Hello\rWorld")
+print("abca\rb")
+print("Loading.\rLoading..\rLoading...")
+print("com\rcomplete")
