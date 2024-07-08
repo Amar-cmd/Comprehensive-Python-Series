8c2d041b817b0f0a0782abd60aa3e9afd912e362
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2024-07-08 21:27:58 +0530

    Add files via upload

---
+++ Unit 3/Code/video3.6.py
@@ -0,0 +1,93 @@
+# ! Addition Operator
+
+# print(5+3) #int
+# print(5+3.03) #float
+# print(5.45+3.03) #float
+# print(5.45+3.03+3j) #complex
+
+# ! Subtraction Operator
+# print(5-3) #int
+# print(5-3.03) #float
+# print(5.45-3.03) #float
+# print(5.45-(3.03+3j)) #complex
+
+# ! Multiplication Operator
+# print(5*3) #int
+# print(5*3.03) #float
+# print(5.45*3.03) #float
+# print(5.45*(3.03+3j)) #complex
+
+
+# ! Division Operator (kya milega)
+# print(10/2)
+# print(10/2.5)
+# print(0/2.5)
+# print(10/0)
+# print((3.03+3j)/3)
+
+
+# ! Modulus (Remainder) Operator (kitna bachega)
+
+# print(10%2)
+# print(10%2.5)
+# print(10.3%2.5)
+# print(10.3%(3.03+3j))
+
+
+# ! Exponent (Raised To) Operator
+
+# print(2**4) #2*2*2*2 = 16
+# print(2.5**2) # 2.5*2.5
+
+# ! Floor Division Operator
+# print(10/2)
+# print(10//2)
+# print(-10//2)
+# print(9//2)
+# print(-9//2)
+# print(10//2.5)
+# print(2.5//10)
+
+
+# ! Precedence and Associativity of Python Arithmetic Operators
+
+# a = 2 + 3 * 4 # Precedence
+# a = 2 ** 3 ** 2 # Associativity
+
+a = 2 + 3 * 4 - 5 / 2
+b = (2 + 3) * (4 - 5) / 2
+c = 10 % 3 * 2 // 4 / 2
+d = 2 ** 4 ** 2 / 10
+e = 5 * 2 / 10 // 5 % 3
+
+print(e)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
