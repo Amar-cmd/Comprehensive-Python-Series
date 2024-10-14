ae279974a94720001c00498493fcc28635f59b7d
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2024-10-14 08:52:25 +0530

    Add files via upload

---
+++ Unit 3/Code/video3.9.py
@@ -0,0 +1,83 @@
+#! Comparison Operators
+
+# ? Equality (==)
+
+# print(a==b)
+# print(a == c)
+
+# ? Inequality (!=)
+# print(a!=b)
+# print(a != c)
+
+
+# a = 10
+# b = 10
+# c = 20
+
+# ? Greater Than (>)
+# print(a > b) # 10 > 10
+# print(c > a) # 20 > 10
+# print(a > c) # 10 > 20
+
+
+# ? Less Than (<)
+# print(a < b) # 10 < 10
+# print(c < b) # 20 < 10
+# print(a < c) # 10 < 20
+
+# ? Greater Than or Equal To (>=)
+# print(a >= b) # 10 >= 10
+# print(a >= c) # 10 >= 20
+# print(c >= a) # 20 >= 10
+
+# ? Less Than or Equal To (<=)
+# print(a <= b) # 10 <= 10
+# print(a <= c) # 10 <= 20
+# print(c <= a) # 20 <= 10
+
+
+# ? Chaining Comparison Operators
+
+# print(3<5<7)
+# print(3<5>4)
+# print(3==5>4)
+#
+# # Quiz
+#
+# print(4<5!=3>4)
+
+
+# ? Comparing Different Data Types
+
+# print(3 == 3.0)
+# print(4.1 <= 5)
+
+# print(3 == "3")
+# print(3.0 != "3")
+# # print(3 < "3")
+# print(3 <= "3")
+
+
+# print("A" == "B")
+# print(ord("A"))
+# print(ord("B"))
+#
+# print("Apple" == "Banana")
+# print("apple" < "Apple")
+# print(ord("a"))
+# print(ord("A"))
+#
+#
+# print("apple" > "apricot")
+#
+# print(ord("a"))
+# print(ord("p"))
+# print(ord("r"))
+
+
+# print(True == 1)
+# print(True == 0)
+# print(False == 0)
+# print(False == 1)
+# print(False != 1)
+# print(True < 2.5)
