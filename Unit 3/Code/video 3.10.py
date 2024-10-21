ba61538fb2210ddf34004594dffa0511eba19b8f
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2024-10-21 12:41:19 +0530

    Add files via upload

---
+++ Unit 3/Code/video 3.10.py
@@ -0,0 +1,90 @@
+#! Logical Operator
+
+# ? AND operation (and)
+# x = 5
+# y = 10
+# if x > 3 and y < 15:
+#     print("Both conditions are true")
+
+
+# ? OR operation (or)
+# x = 5
+# y = 10
+# if x > 10 or y < 15:
+#     print("At Least one condition is true")
+
+
+# ? NOT Operation (not)
+# x = 5
+# if not x > 10:
+#     print("x is not greater than 10.")
+
+# ? Combining Logical Operator
+# x = 5
+# y = 10
+# z = 15
+# if (x < y or y > z) and not (x + y > z):
+#     print("Complex condition is True")
+
+
+# ? Practical Example - 1
+# students = [
+#     {"name": "Alice", "age": 20, "grade": "A"},
+#     {"name": "Bob", "age": 19, "grade": "B"},
+#     {"name": "Charlie", "age": 22, "grade": "C"},
+# ]
+#
+# for student in students:
+#     if student["age"] > 18 and student["grade"] == "A":
+#         print(student["name"])
+
+# ? Practical Example - 2
+# username = input("Enter username: ")
+# password = input("Enter Password: ")
+#
+# if username == "admin" and password == "password":
+#     print("Access Granted")
+# else:
+#     print("Access Not Granted")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
