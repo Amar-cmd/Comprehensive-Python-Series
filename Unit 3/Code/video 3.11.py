f6f55968b7f72739079a5cee3a7df063f1c20fd2
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2024-11-04 09:59:52 +0530

    Add files via upload

---
+++ Unit 3/Code/video 3.11.py
@@ -0,0 +1,55 @@
+# ! In Operator
+
+# print(5 in [1,2,3,4,5])
+# print(3 in (1,2,3))
+# print('a' in {"a", "b", "c"})
+
+# print("Hello" in "Hello Python")
+# print('b' in 'banana')
+# print('s' in 'banana')
+
+
+
+
+# ! Not In Operator
+# print(3 not in [1,2,3])
+# print(5 not in [1,2,3])
+#
+# print(3 not in (1,2,3))
+# print(5 not in (1,2,3))
+
+# print('s' not in 'banana')
+# print('a' not in 'banana')
+#
+# print("hello" not in "hello world")
+# print("python" not in "hello world")
+
+
+
+
+
+# ! Case Sensitivity
+
+# print("h" in "hello")
+# print("h" in "Hello")
+
+print("hello" not in "Hello world")
+print("hello" not in "hello world")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Unit 3/Code/video 3.12.py
@@ -0,0 +1,111 @@
+# ! Identity Operator
+
+# * "Is" Operator
+
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
+
+# print(a is b)
+# print(a is c)
+
+
+# * "Is Not" Operator
+
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
+#
+# print(a is not b)
+# print(a is not c)
+#
+# print("ID of a", id(a))
+# print("ID of b", id(b))
+# print("ID of c", id(c))
+
+
+# * Python Optimization
+
+# ? Integer Caching
+
+a = 10
+b = 10
+# print(a is b)
+# print(a is not b)
+
+
+# ? String Interning
+
+c = "hello"
+d = "hello"
+print(c is d)
+print(c is not d)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # * Identity (is | is not) Vs Equality (== | !=)
+#
+# a = [1, 2, 3]
+# b = a
+# c = [1, 2, 3]
+#
+# print("Checking Identity Operator")
+# print(a is b)
+# print(a is c)
+#
+# print("Checking Equality Operator")
+# print(a == b)
+# print(a == c)
+
+
+
+
+
+
+
+
+
+
+
