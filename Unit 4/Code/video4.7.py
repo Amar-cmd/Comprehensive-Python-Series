32e3b0ff8b7b07f9142d7742e557c23b2000736c
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2025-01-30 10:03:54 +0530

    Add files via upload

---
+++ Unit 4/Code/video4.7.py
@@ -0,0 +1,59 @@
+# ? while loop example
+
+# counter = 1
+#
+# while counter <= 5:
+#     print(counter)
+#     counter += 1
+
+
+# ? while Loop Control example - break
+
+# counter = 1
+#
+# while counter <= 5:
+#     if counter == 3:
+#         break
+#     print(counter)
+#     counter += 1
+
+
+# ? while Loop Control example - continue
+
+# counter = 1
+#
+# while counter <= 5:
+#     if counter == 3:
+#         counter += 1
+#         continue
+#     print(counter)
+#     counter += 1
+
+# ? Infinite Loop example
+
+# while True:
+#     print("This will print forever")
+
+
+# ? while-else Loop example
+
+# counter = 1
+#
+# while counter <= 3:
+#     print(counter)
+#     counter += 1
+#
+# else:
+#     print("Loop Completed")
+
+
+# ? nested while Loop example
+
+# i = 1  # track outer loop
+#
+# while i <= 2:
+#     j = 1  # track inner loop
+#     while j <= 2:
+#         print(i, j)
+#         j += 1  # update inner loop
+#     i += 1  # update outer loop
