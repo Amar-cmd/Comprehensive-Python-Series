c873cf40e83ece750993573755f3823026266a26
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2025-03-10 09:52:46 +0530

    Add files via upload

---
+++ Unit 5/Code/Video 2.py
@@ -0,0 +1,114 @@
+# ! Creating String
+
+# print("""Hello""")
+
+# s = '''This is line 1
+# This is line 2
+# This is line 3'''
+#
+# print(s)
+
+
+# ! Accessing String
+
+# s = "Python"
+
+#  []
+# print(s[0])
+# print(s[2])
+# print(s[5])
+# print(s[len(s) - 1])
+
+# print(s[-1])
+# print(s[-3])
+# print(s[-5])
+# print(len(s))
+#
+# print(s[7])
+
+
+# ! String Slicing (Positive Indexing)
+
+# s = "python"
+#
+# # s[start:end:step]
+#
+# # TODO: extract PYT
+# print(s[0:3:1])
+#
+# # TODO: extract pyth
+# print(s[:4])
+#
+# # TODO: extract thon
+# print(s[2:])
+#
+# # TODO: extract python
+# print(s[:])
+
+
+# ! String Slicing (Negative Indexing)
+
+# s = "python"
+#
+# # TODO: extract tho
+# print(s[-4:-1])
+#
+# # TODO: extract ython
+# print(s[-5:])
+#
+# # TODO: extract pyth
+# print(s[:-2])
+
+
+# ! String Slicing with step (skipping characters)
+
+# s = "python"
+#
+# # TODO: extract pto
+# print(s[::2])
+#
+# # TODO: extract nohtyp
+# print(s[::-1])
+#
+# # TODO: extract nhy
+# print(s[::-2])
+
+
+# ! Iterating over String
+
+# s = "python"
+# i = 0
+# # for i in s:
+# #     print(i)
+#
+# while i < len(s):
+#     print(s[i])
+#     i += 1
+
+
+# ! Concatenation
+
+# s1 = "hello"
+# s2 = "world"
+# s3 = "1"
+#
+# print(s1 + " " + s2)
+# print(s1 + s3)
+
+
+# ! Repetition
+#
+# s = '3'
+#
+# print(s * 5)
+
+
+
+
+
+
+
+
+
+
+
