c493bcf8ca84b8c450df12a8721bff5dc9756047
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2025-03-10 09:57:59 +0530

    Update Video 3.py

--- Unit 5/Code/Video 3.py
+++ Unit 5/Code/Video 3.py
@@ -1,99 +1,81 @@
-# ! Case Conversion
+# ? STRING METHODS
 
+# ! Case Conversion
 # txt = "HELLO, world"
-#
+
 # # Lower Case
 # print(txt.lower())
-#
+
 # # Upper Case
 # print(txt.upper())
-#
+
 # # Capitalize Case
 # print(txt.capitalize())
-#
+
 # # Title Case
 # print(txt.title())
-#
+
 # # Swap Case
 # print(txt.swapcase())
 
+# ------------------------------------------------------------------------------------------------------------
 
 # ! Searching & Checking
 
 # txt = "Hello, World"
 
-# txt = "Hello, World"
-
 # TODO: first position of 'l'
 # find()
-
 # print(txt.find("l"))
 # print(txt.find("p"))
-
 
 # TODO: check if string starts with "Hello"
 # startswith()
 # print(txt.startswith("Hello"))
 # print(txt.startswith("Java"))
 
-
 # TODO: check if ends with 'rld'
 # endswith()
-
 # print(txt.endswith("rld"))
 # print(txt.endswith("wo"))
 
-
 # TODO: replace "world" with "python"
 # replace()
-#
 # print(txt.replace("World", "Python"))
-
 
 # TODO: Last occurance of "o"
 # rfind()
-
 # print(txt.rfind("o"))
 # print(txt.rfind("o", 1, 5))
 
-
 # TODO: Find first occurance of "l"
 # index()
-
 # print(txt.index("l"))
 # print(txt.index("p"))
 
-
 # TODO: Find last occurance of "l"
 # rindex()
-
 # print(txt.rindex("l"))
 # print(txt.rindex("p"))
 
+# ------------------------------------------------------------------------------------------------------------
 
 # ! Splitting & Joining
-
 # txt = "apple, mango, banana"
 
 # TODO: Split this string
 # split()
-
 # print(txt.split(","))
 # print(txt.split(",", 1))
 
-
 # TODO: Demonstrate splitting from right
 # rsplit()
-
 # print(txt.rsplit(","))
 # print(txt.rsplit(",", 1))
 
-
 # TODO: Do partition on ","
 # partition()
-
 # print(txt.partition(","))
-
 
 # TODO: Split each line
 # splitlines()
@@ -101,17 +83,15 @@
 # print(txt.splitlines())
 # print(txt.splitlines(True))
 
-
 # TODO: Join them
 # join()
-
 # print("".join(txt))
 
+# ------------------------------------------------------------------------------------------------------------
 
 # ! String Validation
 
 # * is alphanumeric only?
-
 # print("Python123".isalnum())
 # print("Python@123".isalnum())
 
@@ -123,84 +103,58 @@
 # print("123".isdigit())
 # print("123abc".isdigit())
 
-
 # * is space only?
 # print("  ".isspace())
 # print(" sd ".isspace())
-
 
 # * is lower case?
 # print("abc".islower())
 # print("abcD".islower())
 
-
 # * is upper case?
 # print("AJC".isupper())
 # print("AJCsd".isupper())
-
 
 # * is title case?
 # print("Hi".istitle())
 # print("HiSD".istitle())
 
+# ------------------------------------------------------------------------------------------------------------
 
 # ! String Formatting
 
 # * strip()
-
 # print("    Hello    ".strip())
 # print("###Hello###".strip("#"))
 
 # * lstrip()
 # print("    Hello    ".lstrip())
 
-
 # * rstrip()
 # print("    Hello    ".rstrip())
 
-
 # * center()
-txt = "hello"
+# txt = "hello"
 # print(txt.center(10))
 # print(txt.center(10, "_"))
-
 
 # * ljust()
 # print(txt.ljust(10))
 # print(txt.ljust(10, "."))
 
-
 # * rjust()
-
 # print(txt.rjust(10))
 # print(txt.rjust(10, "."))
-
 
 # * format()
 # name = "Ravi"
 # age = 25
-#
 # print("I am {} and I am {} years old".format(name, age))
 # print("I am {a} and I am {b} years old".format(a = name, b = age))
 
-
 # * Count()
-
 # s = "a b a a c a"
 
 # TODO: how many times 'a' appears?
 # print(s.count("a"))
 # print(s.count("a", 2, 6))
-
-
-
-
-
-
-
-
-
-
-
-
-
