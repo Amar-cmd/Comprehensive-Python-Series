d11e03a0bd0846d100e605169e1661fee86fb457
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2025-04-21 10:19:32 +0530

    Add files via upload

---
+++ Unit 6/PDFs/Code/Video 6.2.py
@@ -0,0 +1,139 @@
+# ! Accessing Elements from a list
+
+# my_list = ["apple", "banana", "cherry", "date"]
+#
+# print(my_list[0])
+# print(my_list[2])
+#
+#
+# # -ve indexing
+# print(my_list[-1])
+# print(my_list[-3])
+
+
+# ! Accessing Elements from a list
+# ? Slicing
+
+
+# my_list = [10, 20, 30, 40, 50, 60]
+#
+# print(my_list[1:4])
+#
+# print(my_list[::2])
+#
+# print(my_list[::-1])
+
+
+
+# ! Modifying a List
+
+# Changing element
+# my_list = ["apple", "banana", "cherry"]
+# my_list[1] = "blueberry"
+# print(my_list)
+#
+#
+# my_list[1:3] = ["papaya", "mango"]
+# print(my_list)
+
+
+
+
+
+# my_list = ["apple", "banana", "cherry"]
+#
+# my_list[1:2] = ["Papaya", "Mango"]
+# print(my_list)
+
+
+# my_list = ["apple", "banana", "cherry"]
+#
+# my_list[1:3] = ["Papaya"]
+# print(my_list)
+
+
+# ! Adding items to a List
+
+# fruits = ["apple", "banana", "cherry"]
+# fruits.append("orange")
+# print(fruits)
+
+
+# ! Adding items to a List
+
+# subjects = ["Math", "Physics", "Biology"]
+# subjects.insert(1, "Chemistry")
+# print(subjects)
+
+
+# subjects = ["Math", "Physics", "Biology"]
+# subjects.insert(-1, "Chemistry")
+# print(subjects)
+
+
+
+# ! Adding items to a List
+
+# list1 = [1, 2, 3]
+# list2 = [4, 5, 6]
+#
+# list1.extend(list2)
+# print(list1)
+
+
+
+# ! Removing items from a List
+
+# fruits = ["apple", "banana", "cherry", "banana"]
+# fruits.remove("pineapple")
+# print(fruits)
+
+
+# ! Removing items from a List
+
+# numbers = [10, 20, 30, 40]
+# print(numbers.pop(1))
+# print(numbers)
+
+
+# ! Removing items from a List
+
+# item = ["pen", "notebook", "eraser"]
+# print(item)
+# item.clear()
+# print(item)
+
+
+# ! Removing items from a List
+
+numbers = [10, 20, 30, 40]
+
+# del numbers[2]
+# print(numbers)
+
+del numbers[1:2]
+print(numbers)
+
+del numbers
+print(numbers)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
