2a61b80c9d5393d84a3524f1e6c291c31050d6ac
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2025-05-05 11:06:11 +0530

    Add files via upload

---
+++ Unit 6/Code/Video 6.8.py
@@ -0,0 +1,81 @@
+#? Sorting in List
+
+#! .sort() method
+# TODO: Sort the list of strings
+# fruits = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# print("Before sorting", fruits)
+#
+# fruits.sort()
+# print(fruits)
+#
+# print()
+
+# TODO: Sort the list of numbers
+
+# numbers = [100, 50, 65, 82, 23]
+# numbers.sort()
+# print(numbers)
+
+# TODO: Sort the list of strings (descending order)
+
+# fruits = ["orange", "mango", "kiwi", "pineapple", "banana"]
+# fruits.sort(reverse=True)
+# print(fruits)
+
+
+# TODO: Sort the list of numbers (descending order)
+
+# numbers = [100, 50, 65, 82, 23]
+#
+# numbers.sort(reverse=True)
+# print(numbers)
+
+#! sorted() function
+# TODO: Sorting Without Changing the Original List
+#
+# numbers = [3, 1, 4, 1, 5, 9, 2]
+# sort = sorted(numbers)
+# print(numbers)
+# print(sort)
+#
+# sort_desc = sorted(numbers, reverse=True)
+# print(sort_desc)
+
+
+# TODO: Sorting based on length of word (using .sort() method)
+
+# words = ["apple", "banana", "cherry", "date"]
+# words.sort(key=len)
+# print(words)
+
+#! Custom Sorting
+# TODO: Sorting based on length of word (using sorted() function)
+
+# words = ["apple", "banana", "cherry", "date"]
+# sort = sorted(words, key=len)
+# print(sort)
+
+
+# TODO: Sorting numbers based on their last digit
+
+# def fun(n):
+#     return n % 10
+#
+# numbers = [100, 51, 65, 82, 23]
+# numbers.sort(key=fun)
+#
+# print(numbers)
+
+
+# TODO: Case-Insensitive Sorting
+
+# lst = ["Mango", "Kiwi", "banana"]
+# lst.sort(key = str.lower)
+# print(lst)
+
+#! Reverse Sorting
+# TODO: Flip the list
+
+# letters = ["A", "d", "f", "E"]
+# letters.reverse()
+# print(letters)
