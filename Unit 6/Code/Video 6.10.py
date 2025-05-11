a96b12d2b4418654b8a4c52dcf9afacc34be9db4
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2025-05-11 20:10:57 +0530

    Add files via upload

---
+++ Unit 6/Code/Video 6.10.py
@@ -0,0 +1,84 @@
+#! List Methods
+# * Adding
+
+#? .append()
+# my_list = [1, 2, 3]
+# my_list.append(4)
+# print(my_list)
+
+#? .extend()
+# my_list.extend([4,5,6])
+# print(my_list)
+
+#? .insert()
+# my_list.insert(1, 10)
+# print(my_list)
+
+
+# * Removing Element
+
+#? .remove()
+# my_list = [1, 2, 3, 2, 4]
+# my_list.remove(2)
+# print(my_list)
+
+#? .pop()
+# my_list.pop(1)
+# print(my_list)
+
+#? .clear()
+# my_list.clear()
+# print(my_list)
+
+
+# * Searching & Counting
+
+# my_list = [1, 2, 3, 2, 4]
+
+#? .index()
+# print(my_list.index(2))
+# print(my_list.index(2, 2))
+
+#? .count()
+# print(my_list.count(2))
+
+
+# * Sorting & Reversing
+
+# my_list = [4, 2, 9, 1]
+
+#? .sort()
+# my_list.sort()
+# my_list.sort(reverse=True)
+# print(my_list)
+
+
+#? sorted()
+# modify = sorted(my_list, reverse=True)
+# print(my_list)
+# print(modify)
+
+#? reverse()
+# my_list.reverse()
+# print(my_list)
+
+
+# * Copying
+
+# my_list = [4, 2, 9, 1]
+
+#? .copy()
+# copy = my_list.copy()
+# copy.append(5)
+
+# print(my_list)
+# print(copy)
+
+
+
+
+
+
+
+
+
