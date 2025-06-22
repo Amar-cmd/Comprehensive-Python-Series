2fa01492256cf29ddc48f1f4c186d9f24ef5b54e
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2025-06-22 13:53:16 +0530

    Add files via upload

---
+++ Unit 7/Code/Video 7.5.py
@@ -0,0 +1,11 @@
+#? Tuple Methods
+
+#* 1. .count()
+my_tuple = (1, 2, 3, 4, 2, 2, 5)
+print(my_tuple.count(2))
+
+
+#* 2. .index()
+my_tuple = (10, 20, 30, 40, 50, 20, 60)
+print(my_tuple.index(20))
+print(my_tuple.index(20, 2, 6))
