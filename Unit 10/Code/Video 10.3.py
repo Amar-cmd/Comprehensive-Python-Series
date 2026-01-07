097a1f7edbf339874302883e91b25c335a54502b
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2026-01-07 07:59:36 +0530

    Add files via upload

---
+++ Unit 10/Code/Video 10.3.py
@@ -0,0 +1,15 @@
+#? Function Annotation
+
+# def add(a: int, b: int) -> int:
+#     print(a+b)
+# add(5, 10)
+# print(add.__annotations__)
+
+#todo: Print sum of list of numbers
+
+# from typing import List
+#
+# def total(num: List[int]) -> int:
+#     return sum(num)
+#
+# print(total([1,2,3,4,5]))
