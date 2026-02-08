0edeab41d02c0749b93ae0692a33d4acd59e6d71
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2026-02-08 09:15:31 -0800

    Add files via upload

---
+++ Unit 11/Code/Video 3.py
@@ -0,0 +1,26 @@
+#! Raising Exception
+
+# age = -5
+# if age < 0:
+#     raise ValueError("Age cannot be less than 0")
+# else:
+#     print(age)
+
+
+
+
+
+
+#! Assertion
+
+def sqrt(n):
+    assert n >= 0, "number must be non-negative"
+    return n ** 0.5
+
+print(sqrt(-4))
+
+
+
+
+
+
