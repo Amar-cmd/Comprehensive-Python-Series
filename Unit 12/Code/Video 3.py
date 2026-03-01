7e3619f594b2dfaa12a330bc125a04ed26d698ca
Amar Jyoti <70061426+Amar-cmd@users.noreply.github.com>
2026-03-01 08:08:18 -0800

    Add files via upload

---
+++ Unit 12/Code/Video 3.py
@@ -0,0 +1,13 @@
+#! Renaming File
+
+# import os
+# os.rename('test', 'sample')
+
+#! Deleting File
+
+# import os
+# if os.path.exists('sample'):
+#     os.remove('sample')
+#     print("File deleted successfully")
+# else:
+#     print("File do not exist")
